957613be5caf492b1d39a3ea7f08215e2be8f4d6
Bhesh Raj Sejawal <bsejawal@gmail.com>
2016-06-29 11:10:13 -0500

    added Order.java

---
+++ src/com/mpp/hr/Order.java
@@ -0,0 +1,40 @@
+package com.mpp.hr;
+
+import java.util.Date;
+
+public class Order {
+	private int orderNo;
+	private Date orderDate;
+	private double orderAmount;
+
+	public Order(int orderNo, Date orderDate, double orderAmount) {
+		this.orderNo = orderNo;
+		this.orderDate = orderDate;
+		this.orderAmount = orderAmount;
+	}
+
+	public int getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public Date getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public double getOrderAmount() {
+		return orderAmount;
+	}
+
+	public void setOrderAmount(double orderAmount) {
+		this.orderAmount = orderAmount;
+	}
+
+}
